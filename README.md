77db9af5d7b4a1fddf09888ab0532476206221fa
Marcio <marciolea@gmail.com>
2022-08-16 21:20:15 -0300

    Edição do README e adição de comentários

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# MARCIO LEANDRO DA SILVA OLIVEIRA
-## Habilidades em Agosto de 2022
-- HTML
-- CSS
-- Javascript
-- Java (Desktop)
-- Bootstrap
-- GIT e GitHub
+# Jogo simples com javascript de adivinhar o tipo de número Seguindo documentação oficial do MDN 
+https://developer.mozilla.org/en-US/
+
+- Digite um número aleatório entre 1 e 100 e clique em enviar palpite, você tem 10 tentativas para adivinhar o número:
+
+Dica: se o palpite foi muito baixo digite um número mais alto, se for alto digite mais baixo.
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-var numeroAleatorio= Math.floor(Math.random() * 100) + 1;
+// A primeira variável — numeroAleatorio — recebe um número aleatório entre 1 e 100, calculado usando um algoritmo matemático.
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
 
-var palpites = document.querySelector('.palpites');
-var ultimoResultado = document.querySelector('.ultimoResultado');
-var baixoOuAlto = document.querySelector('.baixoOuAlto');
+// As três primeiras constantes são feitas para armazenar uma referência aos parágrafos de resultados em nosso HTML e são usadas para inserir valores nos parágrafos posteriormente no código (observe como eles estão dentro de um <div>elemento, que é usado para selecionar todos os três mais tarde para resetar, quando reiniciarmos o jogo):
+const palpites = document.querySelector('.palpites');
+const ultimoResultado = document.querySelector('.ultimoResultado');
+const baixoOuAlto = document.querySelector('.baixoOuAlto');
 
-var envioPalpite = document.querySelector('.envioPalpite');
-var campoPalpite = document.querySelector('.campoPalpite');
+// As próximas duas constantes armazenam referências à entrada de texto do formulário e ao botão de envio e são usadas para controlar o envio do palpite posteriormente.
+const envioPalpite = document.querySelector('.envioPalpite');
+const campoPalpite = document.querySelector('.campoPalpite');
 
-var contagemPalpites = 1;
-var botaoReinicio;
+// Nossas duas variáveis ​​finais armazenam uma contagem de palpites de 1 (usada para registrar quantos palpites o jogador teve) e uma referência a um botão de reset que ainda não existe (mas existirá mais tarde).
+let contagemPalpites = 1;
+let botaoReinicio;
 
 function conferirPalpite() {
-    var palpiteUsuario = Number(campoPalpite.value);
+    // A primeira linha declara uma variável chamada userGuesse define seu valor para o valor atual inserido no campo de texto. Também executamos esse valor por meio do Number()construtor interno, apenas para garantir que o valor seja definitivamente um número. Como não estamos alterando essa variável, vamos declará-la usando const.
+    const palpiteUsuario = Number(campoPalpite.value);
+    // Em seguida, encontramos nosso primeiro bloco de código condicional. Um bloco de código condicional permite que você execute o código seletivamente, dependendo se uma determinada condição é verdadeira ou não. Parece um pouco como uma função, mas não é. A forma mais simples de bloco condicional começa com a palavra-chave if, depois alguns parênteses e algumas chaves. Dentro dos parênteses incluímos um teste. Se o teste retornar true, executamos o código dentro das chaves. Se não, nós não, e passamos para o próximo pedaço de código. Neste caso, o teste está testando se a guessCountvariável é igual a 1(ou seja, se esta é a primeira vez do jogador ou não):
     if (contagemPalpites === 1) {
       palpites.textContent = 'Palpites anteriores: ';
     }
